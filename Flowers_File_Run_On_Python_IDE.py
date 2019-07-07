37cb5943e0c23845295697d8e111e60404cb2526
Thy H. Nguyen <46432072+nguyent2@users.noreply.github.com>
2019-07-06 22:03:16 -0400

    Update Flowers_File_Run_On_Python_IDE.py

--- Flowers_File_Run_On_Python_IDE.py
+++ Flowers_File_Run_On_Python_IDE.py
@@ -1,14 +1,23 @@
 thy = input()
+#This is the place for user to input anything (a string, a number, or a boolean) 
 import turtle
+#Set up turtle to draw
 wn = turtle.Screen()
+#Set up the screen of the turtle
 wn.bgcolor("#E0FFFF")
+#Set up the background color of the turtle
 thuy = turtle.Turtle()
+#Set up the turtle with the name "thuy"
 thuy.color("blue")
-if type(thy) == str and thy =="a":
+#Set up the turtle's color
+if thy =="a":
     thuy.forward(100)
-elif type(thy) == str and thy != "a":
-    thuy.right(100)
-    thuy.forward(100)
+#If the user input the string "a", it will draw a straight line 
 else:
-    print("Please enter a string and try back!")
+    for i in range(10):
+        thuy.right(360/10)
+        thuy.forward(100)
+#If the input is not "a", it will draw the decagon)
+    
 wn.exitonclick()
+#Set up the screen so that it will exit when the user clicks
